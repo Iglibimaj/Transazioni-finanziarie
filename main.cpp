451ffbe57cb5aac29f2357cf63aba5901d8b953a
igli <igli.bimaj@edu.unifi.it>
2025-07-01 18:35:30 +0200

    Correzione errori minori e miglioramento correttezza codice, impossibilità di creare conti con saldo negativo, fare bonifici in negativo o mandare il saldo in negativo attraverso un bonifico

--- main.cpp
+++ main.cpp
@@ -15,34 +15,23 @@ public:
 
     std::map<std::string, AccountInfo> ContoCorrente;
 
-    CC(const std::string& Iban, const std::string& Cognome, const std::string& Nome, float Valore) {
+    CC(const std::string &Iban, const std::string &Cognome, const std::string &Nome, float Valore) {
+        auto it = ContoCorrente.find(Iban);
+        if (Valore < 0) {
+            std::cerr << "Errore nella creazione del conto: Il conto non può essere creato con valore negativo";
+            return;
+        }
+        if (it != ContoCorrente.end()) {
+            std::cerr << "Errore nella creazione del conto corrente: IBAN già in uso";
+            return;
+        }
         ContoCorrente.insert({Iban, {Cognome, Nome, Valore}});
     }
 
-    void bonificoEntrata(const std::string& Iban, float valore) {
+    void searchIban(const std::string &Iban) const {
         auto it = ContoCorrente.find(Iban);
         if (it != ContoCorrente.end()) {
-            it->second.Valore += valore;
-            it->second.fileEntrate.emplace_back(Iban, valore);
-        } else {
-            std::cerr << "Errore: IBAN non trovato per bonifico di entrata.\n";
-        }
-    }
-
-    void bonificoUscita(const std::string& Iban, float valore) {
-        auto it = ContoCorrente.find(Iban);
-        if (it != ContoCorrente.end()) {
-            it->second.Valore -= valore;
-            it->second.fileUscite.emplace_back(Iban, valore);
-        } else {
-            std::cerr << "Errore: IBAN non trovato per bonifico di uscita.\n";
-        }
-    }
-
-    void searchIban(const std::string& Iban) const {
-        auto it = ContoCorrente.find(Iban);
-        if (it != ContoCorrente.end()) {
-            const auto& account = it->second;
+            const auto &account = it->second;
             std::cout << "Il conto corrente del signor/a " << account.Cognome << " " << account.Nome
                       << " in questo momento ha " << account.Valore << " euro.\n";
         } else {
@@ -50,12 +39,48 @@ public:
         }
     }
 
-    void leggiEntrate(const std::string& Iban) const {
+    void bonificoEntrata(const std::string &Iban, float valore) {
+        if (valore <= 0) {
+            std::cerr << "Bonifico non possibile: il bonifico in entrata non può essere negativo o uguale a zero\n";
+            return;
+        }
+            auto it = ContoCorrente.find(Iban);
+            if (it != ContoCorrente.end()) {
+                it->second.Valore += valore;
+                it->second.fileEntrate.emplace_back(Iban, valore);
+            } else {
+                std::cerr << "Bonifico non possibile: IBAN non trovato per bonifico in entrata.\n";
+                return;
+            }
+    }
+
+    void bonificoUscita(const std::string &Iban, float valore) {
+        if (valore <= 0) {
+            std::cerr << "Bonifico non possibile: il bonifico in uscita non può essere negativo o uguale a zero\n";
+            return;
+        } else {
+            auto it = ContoCorrente.find(Iban);
+            if (it != ContoCorrente.end()) {
+                it->second.Valore -= valore;
+                it->second.fileUscite.emplace_back(Iban, valore);
+            } else {
+                std::cerr << "Bonifico non possibile: IBAN non trovato per bonifico in uscita.\n";
+                return;
+            }
+            if (it->second.Valore - valore < 0) {
+                std::cerr << "Bonifico non possibile: il conto andrebbe in negativo";
+                return;
+            }
+        }
+    }
+
+
+    void leggiEntrate(const std::string &Iban) const {
         auto it = ContoCorrente.find(Iban);
         if (it != ContoCorrente.end()) {
-            const auto& account = it->second;
+            const auto &account = it->second;
             std::cout << "Entrate per il conto " << Iban << ":\n";
-            for (const auto& entrate : account.fileEntrate) {
+            for (const auto &entrate: account.fileEntrate) {
                 std::cout << "IBAN: " << entrate.first << ", Valore: " << entrate.second << " euro\n";
             }
         } else {
@@ -63,12 +88,12 @@ public:
         }
     }
 
-    void leggiUscite(const std::string& Iban) const {
+    void leggiUscite(const std::string &Iban) const {
         auto it = ContoCorrente.find(Iban);
         if (it != ContoCorrente.end()) {
-            const auto& account = it->second;
+            const auto &account = it->second;
             std::cout << "Uscite per il conto " << Iban << ":\n";
-            for (const auto& uscite : account.fileUscite) {
+            for (const auto &uscite: account.fileUscite) {
                 std::cout << "IBAN: " << uscite.first << ", Valore: " << uscite.second << " euro\n";
             }
         } else {
@@ -76,24 +101,21 @@ public:
         }
     }
 
-    void saveinFile(const std::string& nomeFile) const {
+    void saveinFile(const std::string &nomeFile) const {
         std::ofstream outFile(nomeFile);
         if (!outFile) {
             std::cerr << "Errore nell'aprire il file.\n";
             return;
         }
-        for (const auto& item : ContoCorrente) {
+        for (const auto &item: ContoCorrente) {
             outFile << item.first << ":"
                     << item.second.Cognome << " "
                     << item.second.Nome << " "
                     << item.second.Valore << "\n";
         }
-        if (!outFile.good()) {
-            std::cerr << "Errore durante la scrittura del file.\n";
-        }
     }
 };
 
-int main(){
+int main() {
     return 0;
 }
